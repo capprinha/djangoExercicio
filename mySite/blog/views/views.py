ca47413458d1ad4413491a43ccbdbd4c5495c734
capprinha <gutoklein21@gmail.com>
2025-12-03 21:17:15 -0300

    add template

--- mySite/blog/views/views.py
+++ mySite/blog/views/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+
+from django.shortcuts import render
 from django.views import generic
 
 class PostView(generic.View):
-    def home(request):
-        return HttpResponse('hello world')
+    def get(self, request):
+        return render(request , 'index.html')
